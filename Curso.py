c9374ec95eb07dd30b0ef2a9c8e59131b09ff3d5
Jared Aguilar <jared291095@gmail.com>
2022-02-11 18:19:20 -0600

    Implementacion de la clase Unico e InventarioCursos

--- Curso.py
+++ Curso.py
@@ -1,52 +1,27 @@
-import datetime
-from FormatoHoras import separadorHoraMinuto
+from abc import ABC,abstractmethod
 
 
-class Curso:
-    nombre = ""
-    nrc = ""
-    codigo = ""
-    horario = dict()
-
-    def __init__(self,nombre,nrc,codigo,dia1,entrada1,salida1,dia2,entrada2,salida2):
-        self.nombre = nombre
-        self.nrc = nrc
-        self.codigo = codigo
-
-        horaE1,minutoE1 = separadorHoraMinuto(entrada1)
-        horaS1,minutoS1 = separadorHoraMinuto(salida1)
-
-        horaE2,minutoE2 = separadorHoraMinuto(entrada2)
-        horaS2,minutoS2 = separadorHoraMinuto(salida2)
-
-        self.horario[dia1]=(datetime.timedelta(hours=horaE1,minutes=minutoE1),datetime.timedelta(hours=horaS1,minutes=minutoS1))
-        self.horario[dia2]=(datetime.timedelta(hours=horaE2,minutes=minutoE2),datetime.timedelta(hours=horaS2,minutes=minutoS2))
-
-    def getNombre(self):
-        return self.nombre
-    def getNrc(self):
-        return self.nrc
-    def getCodigo(self):
-        return self.codigo
-    def getHorario(self):
-        return self.horario
-
-    def mostrarHorario(self):
-        dictionario = self.getHorario()
-        for key in dictionario:
-            print(key,':',dictionario[key][0],'-',dictionario[key][1])
-    
+class Curso(ABC):
+    @abstractmethod
     def toString(self):
-        print(f"Curso: {self.getNombre()}")
-        print(f"NRC: {self.getNrc()}")
-        print(f"Codigo: {self.getCodigo()}")
-        print(f"---Hora del curso---")
-        self.mostrarHorario()
-        print()
-    
+        pass
 
-curso1 = Curso("Programacion 4","41741","EIF209","Lunes","8:00","9:40","Jueves","8:00","9:00")
+    @abstractmethod
+    def __mostrarHorario(self):
+        pass
 
-curso1.toString()
+    def __CursoDeDosDias(self,*args):
+        self.nombre = args[0]
+        self.nrc = args[1]
+        self.codigo = args[2]
+
+        horaE1,minutoE1 = separadorHoraMinuto(args[4])
+        horaS1,minutoS1 = separadorHoraMinuto(args[5])
+
+        horaE2,minutoE2 = separadorHoraMinuto(args[7])
+        horaS2,minutoS2 = separadorHoraMinuto(args[8])
+
+        self.horario[args[3]]=(datetime.timedelta(hours=horaE1,minutes=minutoE1),datetime.timedelta(hours=horaS1,minutes=minutoS1))
+        self.horario[args[6]]=(datetime.timedelta(hours=horaE2,minutes=minutoE2),datetime.timedelta(hours=horaS2,minutes=minutoS2))
 
         
--- FormatoHoras.py
+++ FormatoHoras.py
@@ -1,5 +1,3 @@
-
-
 def separadorHoraMinuto(horario):
     hora = horario.split(':')
     return (int(hora[0]),int(hora[1]))
